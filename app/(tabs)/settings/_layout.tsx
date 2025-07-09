bc54d76f20e1d2d60fa823b0412521e97d71bd93
Damilola Bada <badadamilola@gmail.com>
2025-07-09 20:55:23 +0200

    chore: removed log from layout

--- app/(tabs)/settings/_layout.tsx
+++ app/(tabs)/settings/_layout.tsx
@@ -7,8 +7,6 @@ export default function SettingsStackLayout() {
   const theme = useColorScheme();
   const isDarkMode = theme === 'dark';
 
-  console.log("SettingsStackLayout rendered with theme:", theme);
-
   return (
     <Stack
       screenOptions={{
